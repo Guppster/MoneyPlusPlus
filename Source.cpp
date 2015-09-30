e878b5f90ad0bd2aa992c973d951ce6c87b32a50
Gurpreet Singh <Guppst3r@gmail.com>
2015-09-30 18:25:26 -0400

    Improved Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,39 @@
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	int id;
+	char selection;
 	
-	cout << "Enter your id: \t";
+	cout << "(L)ogin or (R)egister: \t";
+	cin >> selection;
+
+	if(selection=='L' || selection=='l')
+	{
+		login()
+	}
+	else if(selection=="R" || selection == "r")
+	{
+		registerNew();
+	}
+}//End of main method
+
+void login()
+{
+	int id; 
+	string pass;
+
+	cout << "Enter your account ID: \t";
 	cin >> id;
-	
-} 
+
+	cout << "Enter your password: \t";
+	cin >> pass;
+}
+
+void registerNew()
+{
+
+}
